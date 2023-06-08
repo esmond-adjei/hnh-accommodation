a03a436402c26650fd5fd62a27d11219c2a9f533
esmond-adjei <xmongram@gmail.com>
2023-06-08 23:10:11 +0000

    modified some files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo hnh-logo" alt="logo" />
+        <p>
+          A Student Accommodation Webapp. <code>comming soon...</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
