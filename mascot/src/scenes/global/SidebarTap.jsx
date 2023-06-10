d6aa244e6083f97b1efc99a8b37839ea13302278
abudusamad <abudusamed@gmail.com>
2023-06-10 10:19:36 +0000

    mascot
    first

---
+++ mascot/src/scenes/global/SidebarTap.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+
+// const Item = ({ title, to, icon, selected, setSelected }) => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   return (
+//     <MenuItem
+//       active={selected === title}
+//       style={{
+//         color: colors.grey[100],
+//       }}
+//       onClick={() => setSelected(title)}
+//       icon={icon}
+//     >
+//       <Typography>{title}</Typography>
+//       <Link to={to} />
+//     </MenuItem>
+//   );
+// };
+
+const SidebarTap = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <Sidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  HnD Hostel
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <a href="#">Logo</a>
+              </Box>
+
+            </Box>
+          )}
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+           
+          </Box>
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
+};
+
+export default SidebarTap;
