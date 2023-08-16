c83b97a976c05d3da2d0201a1f877a9afbcfe01d
esmond-adjei <xmongram@gmail.com>
2023-08-16 15:36:07 +0000

    modified some files

--- src/components/navELement.jsx
+++ src/components/navELement.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export const NavElement = ({icon, title, handleFunction}) => {
     return (
         <div className="side-nav-icon" onClick={(e) => handleFunction(e)}>
-            <object id="svg-object" aria-label={title} type="image/svg+xml" data={icon} />
+            {/* <object id="svg-object" aria-label={title} type="image/svg+xml" data={icon} /> */}
+            <img src={icon} className="icon" alt={title} />
             <p>{title}</p>
         </div>
     )
